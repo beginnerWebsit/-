ef0268091e54aa1551c9adb14cf3a063c7b9881d
beginnerWebsit <149903025+beginnerWebsit@users.noreply.github.com>
2023-11-05 13:02:36 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+const inputElement = document.getElementById('title')
+const createBtn = document.getElementById('create')
+const listElement = document.getElementById('list')
+const deleteBtn = document.getElementById('delete')
+
+
+const notes = [
+    {
+        title: 'Сюда вы можете вписать что-либо',
+        completed: false,
+    },
+]
+
+function render() {
+    listElement.innerHTML = ''
+    if (notes.length === 0){
+        listElement.innerHTML = '<p>Нет элементов</p>'
+    }
+    for (let i = 0; i < notes.length; i++) {
+        listElement.insertAdjacentHTML('beforeend', getNoteTempLate(notes[i], i))
+    }
+}
+
+render()
+
+createBtn.onclick = function() {
+    if (inputElement.value.length === 0) {
+        return 
+    }
+    const NewNote = {
+        title: inputElement.value,
+        completed: false,
+    }
+    notes.push(NewNote)
+    render()   
+    inputElement.value = ''
+}
+listElement.onclick = function (event) {
+    if (event.target.dataset.index) {
+        const index = parseInt(event.target.dataset.index)
+        const type = event.target.dataset.type
+    
+        if (type === 'toggle') {
+            notes[index].completed = !notes[index].completed
+        }
+        else if (type === 'remove') {
+            notes.splice(index, 1)
+        }
+    }
+    render()
+}
+
+function getNoteTempLate(note, index) {
+    return   `     
+    <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+    > 
+        <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
+        <span>
+            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span>
+            <span class="btn btn-small btn-danger" data-type="remove" data-index="${index}">&times;</span>
+        </span>
+    </li>
+`
+}
